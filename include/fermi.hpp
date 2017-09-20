d312ec324c16b55d930263cc354be0f62fffa15b
Eplistical <gaohan@sas.upenn.edu>
2017-09-20 10:58:36 -0400

    added vector fermi & lorentian

--- include/fermi.hpp
+++ include/fermi.hpp
@@ -1,12 +1,33 @@
 #ifndef _FERMI_HPP
 #define _FERMI_HPP
-// fermi function
+/* fermi function
+ *
+ * f(x) = 1 / (1 + e**x) 
+ */
 
 #include <cmath>
-inline double Fermi(double x){
-	/* f(x) = 1 / (1 + e^x) */
-	x = exp(x);
-	return isinf(x) ? 0.0:1.0 / (1.0 + x);
+#include <vector>
+#include "types.hpp"
+
+inline DOUBLE_T Fermi(DOUBLE_T x) noexcept
+{
+    return 1.0 / (1.0 + std::exp(x));
+}
+
+inline COMPLEX_T Fermi(const COMPLEX_T& z) noexcept
+{
+    return 1.0 / (1.0 + std::exp(x));
+}
+
+template <typename Type>
+inline std::vector<Type> Fermi(const std::vector<Type>& x) noexcept
+{
+    const UINT_T N(x.size());
+    std::vector<Type> rst(N);
+    for (UINT_T i(0); i < N; ++i) {
+        rst[i] = Fermi(x[i]);
+    }
+    return rst;
 }
 
 #endif
--- include/lorentian.hpp
+++ include/lorentian.hpp
@@ -1,11 +1,28 @@
 #ifndef _LORENTIAN_HPP
 #define _LORENTIAN_HPP
-// lorentian function
+/* lorentian function
+ *
+ * L(x) = 1/pi * gamma / (x^2 + gamma^2)
+ */
 
 #include <cmath>
-inline double Lorentian(double x, double gamma){
-	/* L(x) = 1/pi * gamma / (x^2 + gamma^2) */
-	return  gamma / (pow(x, 2) + pow(gamma, 2)) / M_PI;
+#include <vector>
+#include "types.hpp"
+
+inline DOUBLE_T Lorentian(DOUBLE_T x, DOUBLE_T gamma) noexcept
+{
+    const static DOUBLE_T PI_inv(acos(-1.0));
+	return  PI_inv * gamma / (x * x + gamma * gamma);
+}
+
+inline std::vector<DOUBLE_T> Lorentian(const std::vector<DOUBLE_T>& x, DOUBLE_T gamma) noexcept
+{
+    const SIZE_T N(x.size());
+    std::vector<DOUBLE_T> rst(N);
+    for (SIZE_T i(0); i < N; ++i) {
+        rst[i] = Lorentian(x[i], gamma);
+    }
+    return rst;
 }
 
 #endif
