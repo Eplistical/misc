35f380bb0b3a203f3806c7ad50718d2841a4dd5b
Eplistical <gaohan@sas.upenn.edu>
2017-09-28 23:19:18 -0400

    modified format

--- IOer/include/io_base_t.hpp
+++ IOer/include/io_base_t.hpp
@@ -10,91 +10,122 @@
 #include "ioer_macros.hpp"
 #include "ioer_exceptions.hpp"
 
-namespace ioer 
-{
-	using namespace std;
+namespace ioer {
+    using namespace std;
+    const string STDIO_PATH("");
 
-	// path for standard I/O (i.e. screen I/O)
-	const string STDIO_PATH = "" ;
+    class io_base_t 
+    {
+        public:
+            io_base_t() = default;
+            io_base_t(const string& path, ios::openmode mode) noexcept;
+            virtual ~io_base_t() = default;
+            io_base_t(const io_base_t& other) = delete;
+            io_base_t& operator=(const io_base_t& other) = delete;
 
-	// output manager class
-	class io_base_t
-	{
-		protected:
-			unordered_map<string, fstream> _fpdict;
-			// helpers
-			bool path_exist(const string& path) const noexcept
-			{ 
-				return (_fpdict.find(path) != _fpdict.end()); 
-			}
+        public:
+            void open(const string& path, ios::openmode mode);
+            void close(const string& path) noexcept;
 
-			bool is_stdio(const string& path) const noexcept
-			{ 
-				return (path == ioer::STDIO_PATH); 
-			}
+        public:
+            size_t size() const noexcept;
+            iostream& operator[](const string& path);
+            iostream& at(const string& path);
 
-		public:
-			io_base_t() = default;
-			io_base_t(const string& path, ios::openmode mode) noexcept
-			{ 
-				open(path, mode);	
-			}
-			virtual ~io_base_t() = default;
+        protected:
+            unordered_map<string, fstream> _fpdict;
+            bool path_exist(const string& path) const noexcept;
+            bool is_stdio(const string& path) const noexcept;
 
-			// no copy 
-			io_base_t(const io_base_t& other) = delete;
-			io_base_t& operator=(const io_base_t& other) = delete;
+    }; // class io_base_t
 
-			// open/close
-			void open(const string& path, ios::openmode mode)
-			{
-				if (path.size()) {
-					if (path_exist(path))
-						return;
-					_fpdict[path] = fstream(path, mode);
-					if (not _fpdict.at(path).is_open()){
-						ostringstream errmsg;
-						errmsg << "ioer::io_base_t::open : Unable to open file " << path << ".";
-						throw FileNotOpenedError(errmsg.str());
-					}
-				}
-			}
+    /*
+     * Implimentations
+     */
 
-			void close(const string& path) noexcept
-			{
-				if (path != STDIO_PATH) {
-					auto it = _fpdict.find(path);
-					if (it != _fpdict.end()) {
-						it->second.close();
-						_fpdict.erase(it);
-					}
-				}
-			}
+    /*****************************************************************************/
 
-			// getter
-			size_t size(void) const noexcept
-			{ 
-				return _fpdict.size(); 
-			}
+    io_base_t::io_base_t(const string& path, ios::openmode mode) noexcept
+    { 
+        open(path, mode);	
+    }
 
-			// access to ostream object
-			iostream& operator[](const string& path) 
-			{
-				return static_cast<iostream&>
-					( (is_stdio(path) ? cout : _fpdict[path]) );
-			}
+    /*****************************************************************************/
 
-			iostream& at(const string& path)
-			{
-				try {
-					return static_cast<iostream&>
-						( (is_stdio(path) ? cout : _fpdict.at(path)) );
-				} catch(const out_of_range& e) {
-					ostringstream errmsg;
-					errmsg << "ioer::io_base_t::at : " << path << " not opened.";
-					throw FileNotOpenedError(errmsg.str());
-				}
-			}
-	};
-};
-#endif
+    void io_base_t::open(const string& path, ios::openmode mode)
+    {
+        if (path.size()) {
+            if (path_exist(path))
+                return;
+            _fpdict[path] = fstream(path, mode);
+            if (not _fpdict.at(path).is_open()){
+                ostringstream errmsg;
+                errmsg << "ioer::io_base_t::open : Unable to open file "
+                        << path << ".";
+                throw FileNotOpenedError(errmsg.str());
+            }
+        }
+    }
+
+    /*****************************************************************************/
+
+    void io_base_t::close(const string& path) noexcept
+    {
+        if (path != STDIO_PATH) {
+            auto it = _fpdict.find(path);
+            if (it != _fpdict.end()) {
+                it->second.close();
+                _fpdict.erase(it);
+            }
+        }
+    }
+
+    /*****************************************************************************/
+
+    size_t io_base_t::size() const noexcept
+    { 
+        return _fpdict.size(); 
+    }
+
+    /*****************************************************************************/
+
+    iostream& io_base_t::operator[](const string& path) 
+    {
+        return static_cast<iostream&>
+            ( (is_stdio(path) ? cout : _fpdict[path]) );
+    }
+
+    /*****************************************************************************/
+
+    iostream& io_base_t::at(const string& path)
+    {
+        try {
+            return static_cast<iostream&>
+                ( (is_stdio(path) ? cout : _fpdict.at(path)) );
+        } catch(const out_of_range& e) {
+            ostringstream errmsg;
+            errmsg << "ioer::io_base_t::at : "
+                    << path << " not opened.";
+            throw FileNotOpenedError(errmsg.str());
+        }
+    }
+
+    /*****************************************************************************/
+
+    bool io_base_t::path_exist(const string& path) const noexcept
+    { 
+        return (_fpdict.find(path) != _fpdict.end()); 
+    }
+
+    /*****************************************************************************/
+
+    bool io_base_t::is_stdio(const string& path) const noexcept
+    { 
+        return (path == ioer::STDIO_PATH); 
+    }
+
+    /*****************************************************************************/
+
+}; // namespace ioer
+
+#endif // _IOER_IO_BASE_T_HPP
--- IOer/include/ioer_exceptions.hpp
+++ IOer/include/ioer_exceptions.hpp
@@ -7,53 +7,53 @@
 
 namespace ioer
 {
-	// Exceptions
-	class IOError : std::exception
-	{
-		protected:
-			std::string _title;
-			std::string _msg;
-		public:
-			explicit IOError(const std::string& msg = "") :
-				_title("IOError"), 
-				_msg((msg == "")?_title:(_title + ":" + msg)) {}
-			// default destructor
-			virtual ~IOError() = default;
-			virtual const char* what() const noexcept{ return _msg.c_str(); }
-	};
+    // Exceptions
+    class IOError : std::exception
+    {
+        protected:
+            std::string _title;
+            std::string _msg;
+        public:
+            explicit IOError(const std::string& msg = "") :
+                _title("IOError"), 
+                _msg((msg == "")?_title:(_title + ":" + msg)) {}
+            // default destructor
+            virtual ~IOError() = default;
+            virtual const char* what() const noexcept{ return _msg.c_str(); }
+    };
 
-	class FileNotOpenedError : public IOError
-	{
-		public:
-			explicit FileNotOpenedError(const std::string& msg = "")
-			{
-				_title = "FileNotOpenedError"; 
-				_msg = (msg == "")?_title:(_title + ":" + msg);
-			} 
-			~FileNotOpenedError() = default;
-	};
+    class FileNotOpenedError : public IOError
+    {
+        public:
+            explicit FileNotOpenedError(const std::string& msg = "")
+            {
+                _title = "FileNotOpenedError"; 
+                _msg = (msg == "")?_title:(_title + ":" + msg);
+            } 
+            ~FileNotOpenedError() = default;
+    };
 
-	class FileNotExistError : public IOError
-	{
-		public:
-			explicit FileNotExistError(const std::string& msg = "")
-			{
-				_title = "FileNotExistError"; 
-				_msg = (msg == "")?_title:(_title + ":" + msg);
-			} 
-			~FileNotExistError() = default;
-	};
+    class FileNotExistError : public IOError
+    {
+        public:
+            explicit FileNotExistError(const std::string& msg = "")
+            {
+                _title = "FileNotExistError"; 
+                _msg = (msg == "")?_title:(_title + ":" + msg);
+            } 
+            ~FileNotExistError() = default;
+    };
 
-	class FileModeIncompatibleError : public IOError
-	{
-		public:
-			explicit FileModeIncompatibleError(const std::string& msg = "")
-			{
-				_title = "FileModeIncompatibleError"; 
-				_msg = (msg == "")?_title:(_title + ":" + msg);
-			} 
-			~FileModeIncompatibleError() = default;
-	};
+    class FileModeIncompatibleError : public IOError
+    {
+        public:
+            explicit FileModeIncompatibleError(const std::string& msg = "")
+            {
+                _title = "FileModeIncompatibleError"; 
+                _msg = (msg == "")?_title:(_title + ":" + msg);
+            } 
+            ~FileModeIncompatibleError() = default;
+    };
 
 };
 #endif
