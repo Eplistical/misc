35069ca7a1c4c28549e394730bd628b81cd392c0
gaohan <gaohanm@gmail.com>
2018-03-09 15:38:19 -0500

    added choice function for multiple distinct values

--- include/randomer.hpp
+++ include/randomer.hpp
@@ -9,6 +9,9 @@
 #include <cmath>
 #include <random>
 #include <vector>
+#include <unordered_set>
+#include <algorithm>
+#include <bitset>
 #include "types.hpp"
 
 namespace randomer {
@@ -20,10 +23,10 @@ namespace randomer {
 		return dis(gen);
 	}
 
-	inline std::vector<DOUBLE_T> vrand(INT_T N, DOUBLE_T lb = 0.0, DOUBLE_T ub = 1.0) noexcept {
+	inline std::vector<DOUBLE_T> vrand(UINT_T N, DOUBLE_T lb = 0.0, DOUBLE_T ub = 1.0) noexcept {
 		std::uniform_real_distribution<> dis(lb, ub);
 		std::vector<DOUBLE_T> rst(N);
-		for(INT_T j = 0; j < N; ++j) {
+		for(UINT_T j = 0; j < N; ++j) {
 			rst[j] = dis(gen);
 		}
 		return rst;
@@ -34,22 +37,23 @@ namespace randomer {
 		return dis(gen);
 	}
 
-	inline std::vector<DOUBLE_T> vnormal(INT_T N, DOUBLE_T mu = 0.0, DOUBLE_T sigma = 1.0) noexcept {
+	inline std::vector<DOUBLE_T> vnormal(UINT_T N, DOUBLE_T mu = 0.0, DOUBLE_T sigma = 1.0) noexcept {
 		std::normal_distribution<DOUBLE_T> dis(mu, sigma);
 		std::vector<DOUBLE_T> rst(N);
-		for(INT_T j = 0; j < N; ++j) {
+		for(UINT_T j = 0; j < N; ++j) {
 			rst[j] = dis(gen);
 		}
 		return rst;
 	}
 
 	inline INT_T discrete(const std::vector<DOUBLE_T>& prob) noexcept {
-		std::discrete_distribution<INT_T> dis(prob.begin(), prob.end());
+		// pick one number from 0 - N-1, according to probability prob
+		std::discrete_distribution<> dis(prob.begin(), prob.end());
 		return dis(gen);
 	}
 
     inline INT_T choice(INT_T N) noexcept {
-        // pick one number from 0 to N-1
+        // randomly pick one number from 0 to N-1
         std::uniform_int_distribution<> dis(0, N - 1);
         return dis(gen);
     }
@@ -59,5 +63,76 @@ namespace randomer {
             // pick one element from vector v
             return v.at(choice(v.size()));
         }
+
+	inline std::vector<INT_T> __choice_hash(INT_T N, INT_T m) noexcept {
+		// randomly pick m numbers from 0 to N - 1, Hashset way
+        std::uniform_int_distribution<> dis(0, N - 1);
+		std::unordered_set<INT_T> record;
+		if (2 * m <= N) {
+			while (record.size() < m) {
+				record.insert(dis(gen));
+			}
+			std::vector<INT_T> rst(record.begin(), record.end());
+			std::sort(rst.begin(), rst.end());
+			return rst;
+		}
+		else {
+			m = N - m;
+			while (record.size() < m) {
+				record.insert(dis(gen));
+			}
+			m = N - m;
+			std::vector<INT_T> rst(m);
+			INT_T j(0);
+			for (INT_T i(0); i < N; ++i) {
+				if (record.find(i) == record.end()) {
+					rst[j] = i;
+					++j;
+				}
+			}
+			return rst;
+		}
+	}
+
+	inline std::vector<INT_T> __choice_bit(INT_T N, INT_T m) noexcept {
+		// randomly pick m numbers from 0 to N - 1, Bitset way
+        std::uniform_int_distribution<> dis(0, N - 1);
+		constexpr INT_T BitSetMax(1000000);
+		if (N <= BitSetMax) {
+			std::bitset<BitSetMax> record;
+			std::vector<INT_T> rst(m);
+
+			UINT_T Npick( 2 * m <= N ? m : N - m );
+
+			INT_T j(0);
+			while (j < Npick) {
+				INT_T tmp(dis(gen));
+				if (!record.test(tmp)) {
+					record.set(tmp);
+					++j;
+				}
+			}
+
+			j = 0;
+			BOOL_T target_flag(2 * m <= N);
+			for (INT_T i(0); i < N; ++i) {
+				if (record.test(i) == target_flag) {
+					rst[j] = i;
+					++j;
+				}
+			}
+			return rst;
+		}
+	}
+
+	inline std::vector<INT_T> choice(INT_T N, INT_T m) noexcept {
+		if (N > 1000000 or static_cast<DOUBLE_T>(N) / m > 1000.0) {
+			return __choice_hash(N, m);
+		}
+		else {
+			return __choice_bit(N, m);
+		}
+	}
+	
 };
 #endif // _RANDOMER_HPP
