b4d1f893ce2666c194c069e93fdca27364c88a99
Eplistical <gaohan@sas.upenn.edu>
2018-03-23 11:27:02 -0400

    fixed a bug

--- misc_test/test_positive_mod.cpp
+++ misc_test/test_positive_mod.cpp
@@ -11,20 +11,19 @@ int main(int argc, char** argv) {
 	assert(positive_mod(3, 7) == 3);
 	assert(positive_mod(-3, 7) == 4);
 
-
 	vector<double> numer {-5.0, -3.0, -1.0, 1.0, 3.0, 5.0};
 	vector<int> q;
 	vector<double> r(positive_mod(numer, 1.5, q));
 
-	for (int (i); i < numer.size(); ++i) {
-		assert(r[i] >= 0.0);
+	for (int i(0); i < numer.size(); ++i) {
+		assert(r.at(i) >= 0.0);
 		assert(numer[i] == q[i] * 1.5 + r[i]);
 	}
 
 	vector<double> denum {1.5, 2.5, 0.7, 3.9, 2.1, 3.3};
 	r = positive_mod(numer, denum, q);
 
-	for (int (i); i < numer.size(); ++i) {
+	for (int i(0); i < numer.size(); ++i) {
 		assert(r[i] >= 0.0);
 		assert(numer[i] == q[i] * denum[i] + r[i]);
 	}
