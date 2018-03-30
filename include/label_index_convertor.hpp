67735e4bf67254dc347063046f19d796a698e452
Eplistical <gaohan@sas.upenn.edu>
2018-03-30 12:57:08 -0400

    save

--- include/label_index_convertor.hpp
+++ include/label_index_convertor.hpp
@@ -10,28 +10,40 @@ namespace misc {
 
     class LabelIndexConverter {
         public:
-            explicit LabelIndexConverter(const std::vector<INT_T>& N_per_row) noexcept : 
-                dim_(N_per_row.size()),
-                N_per_row_(N_per_row)
-                {
-                    factors.reserve(dim_);
-                    INT_T tmp(1);
-                    for (INT_Ti(0); i < dim_; ++i) {
-                        factors.push_back(tmp);
-                        tmp *= N_per_row.at(i);
-                    }
-                    maxindex_ = tmp;
-                }
+            LabelIndexConverter() noexcept
+            {
+            }
+
+            LabelIndexConverter(const std::vector<INT_T>& N_per_row) noexcept
+            {
+                init(N_per_row);
+            }
             ~LabelIndexConverter() noexcept = default;
 
         public:
+            VOID_T init(const std::vector<INT_T>& N_per_row) noexcept
+            {
+                dim_ = N_per_row.size();
+                N_per_row_ = N_per_row;
+
+                factors.clear();
+                factors.reserve(dim_);
+                INT_T tmp(1);
+                for (INT_T i(0); i < dim_; ++i) {
+                    factors.push_back(tmp);
+                    tmp *= N_per_row.at(i);
+                }
+                maxindex_ = tmp;
+            }
+
+        public:
             INT_T get_dim() const noexcept { return dim_; }
-            vector<INT_T> get_N_per_row() const noexcept { return N_per_row_; }
+            std::vector<INT_T> get_N_per_row() const noexcept { return N_per_row_; }
 
         public:
             INT_T label_to_index(const std::vector<INT_T>& label) const {
                 INT_T index(0);
-                for (INT_Ti(0); i < dim_; ++i) {
+                for (INT_T i(0); i < dim_; ++i) {
                     assert(label[i] < N_per_row_[i]);
                     index += label[i] * factors[i];
                 }
@@ -40,8 +52,8 @@ namespace misc {
 
             std::vector<INT_T> index_to_label(INT_T index) const {
                 assert(index < maxindex_);
-                vector<INT_T> label(dim_);
-                for (INT_Ti(dim_ - 1); i >= 0; --i) {
+                std::vector<INT_T> label(dim_);
+                for (INT_T i(dim_ - 1); i >= 0; --i) {
                     label[i] = index / factors[i];
                     index %= factors[i];
                 }
@@ -49,8 +61,8 @@ namespace misc {
             }
 
         private:
-            const INT_T dim_;
-            const std::vector<INT_T> N_per_row_;
+            INT_T dim_;
+            std::vector<INT_T> N_per_row_;
             std::vector<INT_T> factors;
             INT_T maxindex_;
     };
