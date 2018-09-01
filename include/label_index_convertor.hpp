7bf8c4ff6508af96aeb6267469119f82aaf7bd75
gaohan <gaohanm@gmail.com>
2018-09-01 09:40:27 -0400

    modified label_index_convertor to a template

--- include/label_index_convertor.hpp
+++ include/label_index_convertor.hpp
@@ -9,64 +9,65 @@
 
 namespace misc {
 
-    class LabelIndexConverter {
-        public:
-            LabelIndexConverter() noexcept
-            {
-            }
+	template <typename EleType>
+		class LabelIndexConverter {
+			public:
+				LabelIndexConverter() noexcept
+				{
+				}
 
-            LabelIndexConverter(const std::vector<INT_T>& N_per_row) noexcept
-            {
-                init(N_per_row);
-            }
-            ~LabelIndexConverter() noexcept = default;
+				LabelIndexConverter(const std::vector<EleType>& N_per_row) noexcept
+				{
+					init(N_per_row);
+				}
+				~LabelIndexConverter() noexcept = default;
 
-        public:
-            VOID_T init(const std::vector<INT_T>& N_per_row) noexcept
-            {
-                dim_ = N_per_row.size();
-                N_per_row_ = N_per_row;
+			public:
+				VOID_T init(const std::vector<EleType>& N_per_row) noexcept
+				{
+					dim_ = N_per_row.size();
+					N_per_row_ = N_per_row;
 
-                factors.clear();
-                factors.reserve(dim_);
-                INT_T tmp(1);
-                for (INT_T i(0); i < dim_; ++i) {
-                    factors.push_back(tmp);
-                    tmp *= N_per_row.at(i);
-                }
-                maxindex_ = tmp;
-            }
+					factors.clear();
+					factors.reserve(dim_);
+					EleType tmp(1);
+					for (EleType i(0); i < dim_; ++i) {
+						factors.push_back(tmp);
+						tmp *= N_per_row.at(i);
+					}
+					maxindex_ = tmp;
+				}
 
-        public:
-            INT_T get_dim() const noexcept { return dim_; }
-            std::vector<INT_T> get_N_per_row() const noexcept { return N_per_row_; }
+			public:
+				EleType get_dim() const noexcept { return dim_; }
+				std::vector<EleType> get_N_per_row() const noexcept { return N_per_row_; }
 
-        public:
-            INT_T label_to_index(const std::vector<INT_T>& label) const {
-                INT_T index(0);
-                for (INT_T i(0); i < dim_; ++i) {
-					crasher::confirm<std::out_of_range>(label[i] < N_per_row_[i], "label_to_index: label outflow");
-                    index += label[i] * factors[i];
-                }
-                return index;
-            }
+			public:
+				EleType label_to_index(const std::vector<EleType>& label) const {
+					EleType index(0);
+					for (EleType i(0); i < dim_; ++i) {
+						crasher::confirm<std::out_of_range>(label[i] < N_per_row_[i], "label_to_index: label outflow");
+						index += label[i] * factors[i];
+					}
+					return index;
+				}
 
-            std::vector<INT_T> index_to_label(INT_T index) const {
-				crasher::confirm<std::out_of_range>(index < maxindex_, "index_to_label: index outflow");
-                std::vector<INT_T> label(dim_);
-                for (INT_T i(dim_ - 1); i >= 0; --i) {
-                    label[i] = index / factors[i];
-                    index %= factors[i];
-                }
-                return label;
-            }
+				std::vector<EleType> index_to_label(EleType index) const {
+					crasher::confirm<std::out_of_range>(index < maxindex_, "index_to_label: index outflow");
+					std::vector<EleType> label(dim_);
+					for (EleType i(dim_ - 1); i >= 0; --i) {
+						label[i] = index / factors[i];
+						index %= factors[i];
+					}
+					return label;
+				}
 
-        private:
-            INT_T dim_;
-            std::vector<INT_T> N_per_row_;
-            std::vector<INT_T> factors;
-            INT_T maxindex_;
-    };
+			private:
+				EleType dim_;
+				std::vector<EleType> N_per_row_;
+				std::vector<EleType> factors;
+				EleType maxindex_;
+		};
 
 };
 
