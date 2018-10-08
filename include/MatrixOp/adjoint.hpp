631d6e463bd11ec2d9afb764801d69e4f0a0648b
gaohan <gaohanm@gmail.com>
2018-10-08 16:17:34 -0400

    added adjoint

---
+++ include/MatrixOp/adjoint.hpp
@@ -0,0 +1,86 @@
+#ifndef _MATRIXOP_ADJOINT_HPP
+#define _MATRIXOP_ADJOINT_HPP
+
+/*  
+ *  matrixop::transpose(A, M)
+ *
+ *  function for caluclating matrix transpose
+ *
+ * 	param   A:  matrix (M*N)
+ * 	param   M:  number of rows in A
+ *
+ * 	return A.T
+ *
+ ****************************************
+ *
+ *  matrixop::adjoint(A, M)
+ *
+ *  function for caluclating matrix adjoint
+ *
+ * 	param   A:  matrix (M*N)
+ * 	param   M:  number of rows in A
+ *
+ * 	return A.H
+ *
+ */
+
+
+#include <vector>
+#include <complex>
+#include "matrixop_config.hpp"
+
+namespace matrixop {
+	using std::vector;
+	using std::complex;
+	using std::conj;
+
+    template <typename T> 
+        inline vector<T> _transpose(const vector<T>& A, CNST_ITYPE M, CNST_ITYPE N)
+        {
+            vector<T> rst(M * N);
+            for (ITYPE r(0); r < M; ++r) {
+                for (ITYPE c(0); c < N; ++c) {
+                    rst[c + r * N] = A[r + c * M];
+                }
+            }
+            return rst;
+        }
+
+    template <typename T> 
+        inline vector< complex<T> > _adjoint(const vector< complex<T> >& A, CNST_ITYPE M, CNST_ITYPE N)
+        {
+            vector< complex<T> > rst(M * N);
+            for (ITYPE r(0); r < M; ++r) {
+                for (ITYPE c(0); c < N; ++c) {
+                    rst[c + r * N] = conj(A[r + c * M]);
+                }
+            }
+            return rst;
+        }
+
+    inline vector<STYPE> _adjoint(const vector<STYPE>& A, CNST_ITYPE M, CNST_ITYPE N)
+    {
+        return _transpose(A);
+    }
+
+    inline vector<DTYPE> _adjoint(const vector<DTYPE>& A, CNST_ITYPE M, CNST_ITYPE N)
+    {
+        return _transpose(A);
+    }
+
+    // interfaces
+    template<typename T>
+        inline vector<T> transpose(const vector<T>& A, CNST_ITYPE M) {
+            CNST_ITYPE N(static_cast<ITYPE>(A.size() / M));
+            return _transpose(A, M, N);
+        }
+
+    template<typename T>
+        inline vector<T> transpose(const vector<T>& A, CNST_ITYPE M) {
+            CNST_ITYPE N(static_cast<ITYPE>(A.size() / M));
+            return _adjoint(A, M, N);
+        }
+
+};
+
+#endif // _MATRIXOP_ADJOINT_HPP
