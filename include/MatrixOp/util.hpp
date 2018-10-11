70e617a8819e0665ea85f8227c7d85ee0e78c09e
gaohan <gaohanm@gmail.com>
2018-10-11 13:01:13 -0400

    added MatrixOp/util.hpp

---
+++ include/MatrixOp/util.hpp
@@ -0,0 +1,61 @@
+#ifndef _MATRIXOP_UTIL_HPP
+#define _MATRIXOP_UTIL_HPP
+
+/*  
+ *  Addtional utilities for MatrixOp
+ *
+ *  matrixop::vecCmatvec(V, A, W)
+ *
+ *  function for calculating <V|A|W>
+ *
+ * 	param   V:  vector V (M)
+ * 	param   A:  matrix A (M*N)
+ * 	param   W:  vector W (N)
+ *
+ * 	return <V|A|W> (scalar)
+ *
+ *
+ ****************************************
+ *
+ *  matrixop::matCmatmat(A, B, C, L, K)
+ *
+ *  function for calculating A' * B * C
+ *
+ * 	param   A:  matrix A (M*L)
+ * 	param   B:  matrix B (L*K)
+ * 	param   C:  matrix C (K*N)
+ * 	param   L, K: matrix B dimensions
+ *
+ * 	return A' * B * C (M*N)
+ *
+ *
+ ****************************************
+ *  
+ */
+
+#include <vector>
+#include <cmath>
+#include "matrixop_config.hpp"
+#include "matvec.hpp"
+#include "matmat.hpp"
+#include "matCmat.hpp"
+#include "dot.hpp"
+
+
+namespace matrixop {
+	using std::vector;
+
+    template <typename T>
+        inline T vecCmatvec(const vector<T>& V, const vector<T>& A, const vector<T>& W) {
+            return dotc(V, matvec(A, W));
+        }
+
+
+    template <typename T>
+        inline T vecCmatvec(const vector<T>& A, const vector<T>& B, const vector<T>& C, CNST_ITYPE L, CNST_ITYPE K) {
+            return matCmat(A, matmat(B, C, K), L);
+        }
+
+};
+
+#endif
