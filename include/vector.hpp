f5594d1a5d497ce9ca73183168c97e7a6421bf57
gaohan <gaohanm@gmail.com>
2018-10-15 13:35:03 -0400

    added conj() helper function, rewrite norm, added conj for vectors

--- include/vector.hpp
+++ include/vector.hpp
@@ -24,6 +24,316 @@ void assertsize(const std::vector<T1>& v1, const std::vector<T2>& v2)
 			"assertsize: v1 & v2 have different sizes!");
 }
 
+template <typename T>
+typename std::enable_if< std::is_arithmetic<T>::value, std::complex<T> >::type
+conj(const std::complex<T>& a) {
+	return std::conj(a);
+}
+
+template <typename T>
+typename std::enable_if< std::is_arithmetic<T>::value, T >::type
+conj(const T& a) {
+	return a;
+}
+
+// element-wise math operations
+
+template <typename T>
+std::vector<T> conj(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = conj(v[j]) ;
+    return rst;
+}
+
+template <typename T>
+std::vector<T> abs(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::abs(v[j]) ;
+    return rst;
+}
+
+template <typename T>
+std::vector<T> sqrt(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+	std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::sqrt(v[j]);
+    return rst;
+}
+
+template <typename T1, typename T2>
+auto pow(const std::vector<T1>& v1, const std::vector<T2>& v2) -> std::vector<decltype(std::pow(v1.at(0), v2.at(0)))>
+{
+	// pow(vec, vec)
+    assertsize(v1, v2);
+    const SIZE_T N(v1.size());
+    std::vector<decltype(std::pow(v1.at(0), v2.at(0)))> rst(N);
+    for(size_t j(0); j < N; ++j)
+		rst[j] = std::pow(v1[j], v2[j]);
+	return rst;
+}
+
+template <typename T1, typename T2>
+auto pow(const std::vector<T1>& v1, const T2& a) -> std::vector<decltype(std::pow(v1.at(0), a))>
+{
+	// pow(vec, scal)
+    const SIZE_T N(v1.size());
+    std::vector<decltype(std::pow(v1.at(0), a))> rst(N);
+    for(size_t j(0); j < N; ++j)
+		rst[j] = std::pow(v1[j], a);
+	return rst;
+}
+
+template <typename T1, typename T2>
+auto pow(const T2& a, const std::vector<T1>& v1) -> std::vector<decltype(std::pow(a, v1.at(0)))>
+{
+	// pow(scal, vec)
+    const SIZE_T N(v1.size());
+    std::vector<decltype(a, std::pow(v1.at(0)))> rst(N);
+    for(size_t j(0); j < N; ++j)
+		rst[j] = std::pow(a, v1[j]);
+	return rst;
+}
+
+template <typename T>
+std::vector<T> exp(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::exp(v[j]) ;
+    return rst;
+}
+
+template <typename T>
+std::vector<T> log(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::log(v[j]) ;
+    return rst;
+}
+
+template <typename T>
+std::vector<T> log2(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::log2(v[j]) ;
+    return rst;
+}
+
+template <typename T>
+std::vector<T> log10(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::log10(v[j]) ;
+    return rst;
+}
+
+template <typename T>
+std::vector<T> sin(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::sin(v[j]) ;
+    return rst;
+}
+
+template <typename T>
+std::vector<T> cos(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::cos(v[j]) ;
+    return rst;
+}
+
+template <typename T>
+std::vector<T> tan(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::tan(v[j]) ;
+    return rst;
+}
+
+template <typename T>
+std::vector<T> asin(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::asin(v[j]) ;
+    return rst;
+}
+
+template <typename T>
+std::vector<T> acos(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::acos(v[j]) ;
+    return rst;
+}
+
+template <typename T>
+std::vector<T> atan(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::atan(v[j]) ;
+    return rst;
+}
+
+template <typename T>
+std::vector<T> sinh(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::sinh(v[j]) ;
+    return rst;
+}
+
+template <typename T>
+std::vector<T> cosh(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::cosh(v[j]) ;
+    return rst;
+}
+
+template <typename T>
+std::vector<T> tanh(const std::vector<T>& v)
+{
+    const SIZE_T N(v.size());
+    std::vector<T> rst(N);
+    for(SIZE_T j(0); j < N; ++j) 
+		rst[j] = std::tanh(v[j]) ;
+    return rst;
+}
+
+// collective operations
+
+template <typename T>
+T sum(const std::vector<T>& v)
+{
+	// return v[0] + v[1] + ... + v[N-1]
+    T rst = static_cast<T>(0);
+    const SIZE_T N(v.size());
+    for(SIZE_T j(0); j < N; ++j) 
+		rst += v.at(j);
+    return rst;
+}
+
+template <typename T>
+T mean(const std::vector<T>& v)
+{
+	// find the mean value of all elements in the std::vector
+    return sum(v) / v.size();
+}
+
+template <typename T>
+T prod(const std::vector<T>& v)
+{
+	// return v[0] * v[1] * ... * v[N-1]
+    T rst = static_cast<T>(1);
+    const SIZE_T N(v.size());
+    for(SIZE_T j(0); j < N; ++j) 
+		rst *= v[j];
+    return rst;
+}
+
+template <typename T>
+typename std::enable_if< std::is_integral<T>::value, DOUBLE_T>::type
+norm(const std::vector<T>& v)
+{
+	// norm of the std::vector<T>
+	DOUBLE_T rst(0.0);
+    const SIZE_T N(v.size());
+	for(SIZE_T j(0); j < N; ++j)
+		rst += v[j] * v[j];
+    return std::sqrt(rst);
+}
+
+template <typename T>
+typename std::enable_if< std::is_floating_point<T>::value, T>::type
+norm(const std::vector<T>& v)
+{
+	// norm of the std::vector<T>
+	T rst(0.0);
+    const SIZE_T N(v.size());
+	for(SIZE_T j(0); j < N; ++j)
+		rst += v[j] * v[j];
+    return std::sqrt(rst);
+}
+
+template <typename T>
+typename std::enable_if< std::is_integral<T>::value, DOUBLE_T>::type
+norm(const std::vector< std::complex<T> >& v)
+{
+	// norm of the std::vector< complex<T> >
+	DOUBLE_T rst(0.0);
+    const SIZE_T N(v.size());
+	for(SIZE_T j(0); j < N; ++j)
+		rst += std::pow(v[j].real(), 2) + pow(v[j].imag(), 2);
+    return std::sqrt(rst);
+}
+
+template <typename T>
+typename std::enable_if< std::is_floating_point<T>::value, T>::type
+norm(const std::vector< std::complex<T> >& v)
+{
+	// norm of the std::vector< complex<T> >
+	T rst(0.0);
+    const SIZE_T N(v.size());
+	for(SIZE_T j(0); j < N; ++j)
+		rst += std::pow(v[j].real(), 2) + pow(v[j].imag(), 2);
+    return std::sqrt(rst);
+}
+
+
+template <typename T>
+T min(const std::vector<T>& v)
+{
+	// find the min element in the std::vector 
+    const SIZE_T N(v.size());
+    T rst = v[0];
+    for(SIZE_T j(1); j < N; ++j)
+        rst = (v[j] < rst)?v[j]:rst;
+    return rst;
+}
+
+template <typename T>
+T max(const std::vector<T>& v)
+{
+	// find the max element in the std::vector 
+    const SIZE_T N(v.size());
+    T rst = v[0];
+    for(SIZE_T j(1); j < N; ++j)
+        rst = (v[j] > rst)?v[j]:rst;
+    return rst;
+}
+
 // arithmetic operators 
 
 template <typename T1, typename T2>
@@ -600,34 +910,21 @@ template <typename T1, typename T2>
 auto operator^(const std::vector<T1>& v1, const std::vector<T2>& v2) -> std::vector<decltype(std::pow(v1.at(0), v2.at(0)))>
 {
 	// vec^vec
-    assertsize(v1, v2);
-    const SIZE_T N(v1.size());
-    std::vector<decltype(std::pow(v1.at(0), v2.at(0)))> rst(N);
-    for(size_t j(0); j < N; ++j)
-		rst[j] = std::pow(v1[j], v2[j]);
-	return rst;
+	return pow(v1, v2);
 }
 
 template <typename T1, typename T2>
 auto operator^(const std::vector<T1>& v1, const T2& a) -> std::vector<decltype(std::pow(v1.at(0), a))>
 {
 	// vec^scal
-    const SIZE_T N(v1.size());
-    std::vector<decltype(std::pow(v1.at(0), a))> rst(N);
-    for(size_t j(0); j < N; ++j)
-		rst[j] = std::pow(v1[j], a);
-	return rst;
+	return pow(v1, a);
 }
 
 template <typename T1, typename T2>
 auto operator^(const T2& a, const std::vector<T1>& v1) -> std::vector<decltype(std::pow(a, v1.at(0)))>
 {
 	// scal^vec
-    const SIZE_T N(v1.size());
-    std::vector<decltype(a, std::pow(v1.at(0)))> rst(N);
-    for(size_t j(0); j < N; ++j)
-		rst[j] = std::pow(a, v1[j]);
-	return rst;
+	return pow(a, v1);
 }
 
 template <typename T1, typename T2>
@@ -758,226 +1055,14 @@ operator>>=(std::vector<T1>& v1, const T2& a)
 	return v1;
 }
 
-// element-wise math operations
 
-template <typename T>
-std::vector<T> abs(const std::vector<T>& v)
+template <typename T1>
+std::vector<T1> operator~(const std::vector<T1>& v1)
 {
-    const SIZE_T N(v.size());
-    std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::abs(v[j]) ;
-    return rst;
-}
-
-template <typename T>
-std::vector<T> sqrt(const std::vector<T>& v)
-{
-    const SIZE_T N(v.size());
-	std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::sqrt(v[j]);
-    return rst;
-}
-
-template <typename T1, typename T2>
-auto pow(const std::vector<T1>& v1, const T2& a) -> std::vector<decltype(std::pow(v1.at(0), a))>
-{
-	return v1^a;
-}
-
-template <typename T>
-std::vector<T> exp(const std::vector<T>& v)
-{
-    const SIZE_T N(v.size());
-    std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::exp(v[j]) ;
-    return rst;
-}
-
-template <typename T>
-std::vector<T> log(const std::vector<T>& v)
-{
-    const SIZE_T N(v.size());
-    std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::log(v[j]) ;
-    return rst;
-}
-
-template <typename T>
-std::vector<T> log2(const std::vector<T>& v)
-{
-    const SIZE_T N(v.size());
-    std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::log2(v[j]) ;
-    return rst;
-}
-
-template <typename T>
-std::vector<T> log10(const std::vector<T>& v)
-{
-    const SIZE_T N(v.size());
-    std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::log10(v[j]) ;
-    return rst;
-}
-
-template <typename T>
-std::vector<T> sin(const std::vector<T>& v)
-{
-    const SIZE_T N(v.size());
-    std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::sin(v[j]) ;
-    return rst;
-}
-
-template <typename T>
-std::vector<T> cos(const std::vector<T>& v)
-{
-    const SIZE_T N(v.size());
-    std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::cos(v[j]) ;
-    return rst;
-}
-
-template <typename T>
-std::vector<T> tan(const std::vector<T>& v)
-{
-    const SIZE_T N(v.size());
-    std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::tan(v[j]) ;
-    return rst;
-}
-
-template <typename T>
-std::vector<T> asin(const std::vector<T>& v)
-{
-    const SIZE_T N(v.size());
-    std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::asin(v[j]) ;
-    return rst;
-}
-
-template <typename T>
-std::vector<T> acos(const std::vector<T>& v)
-{
-    const SIZE_T N(v.size());
-    std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::acos(v[j]) ;
-    return rst;
-}
-
-template <typename T>
-std::vector<T> atan(const std::vector<T>& v)
-{
-    const SIZE_T N(v.size());
-    std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::atan(v[j]) ;
-    return rst;
-}
-
-template <typename T>
-std::vector<T> sinh(const std::vector<T>& v)
-{
-    const SIZE_T N(v.size());
-    std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::sinh(v[j]) ;
-    return rst;
-}
-
-template <typename T>
-std::vector<T> cosh(const std::vector<T>& v)
-{
-    const SIZE_T N(v.size());
-    std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::cosh(v[j]) ;
-    return rst;
-}
-
-template <typename T>
-std::vector<T> tanh(const std::vector<T>& v)
-{
-    const SIZE_T N(v.size());
-    std::vector<T> rst(N);
-    for(SIZE_T j(0); j < N; ++j) 
-		rst[j] = std::tanh(v[j]) ;
-    return rst;
-}
-
-// collective operations
-
-template <typename T>
-T sum(const std::vector<T>& v)
-{
-	// return v[0] + v[1] + ... + v[N-1]
-    T rst = static_cast<T>(0);
-    const SIZE_T N(v.size());
-    for(SIZE_T j(0); j < N; ++j) 
-		rst += v.at(j);
-    return rst;
-}
-
-template <typename T>
-T mean(const std::vector<T>& v)
-{
-	// find the mean value of all elements in the std::vector
-    return sum(v) / v.size();
-}
-
-template <typename T>
-T prod(const std::vector<T>& v)
-{
-	// return v[0] * v[1] * ... * v[N-1]
-    T rst = static_cast<T>(1);
-    const SIZE_T N(v.size());
-    for(SIZE_T j(0); j < N; ++j) 
-		rst *= v[j];
-    return rst;
-}
-
-template <typename T>
-auto norm(const std::vector<T>& v) -> decltype(std::abs(v.at(0)))
-{
-	// norm of the std::vector
-	T rst = 0.0;
-    const SIZE_T N(v.size());
-	for(SIZE_T j(0); j < N; ++j)
-		rst += std::pow(std::abs(v[j]), 2);
-    return std::sqrt(rst);
-}
-
-template <typename T>
-T min(const std::vector<T>& v)
-{
-	// find the min element in the std::vector 
-    const SIZE_T N(v.size());
-    T rst = v[0];
-    for(SIZE_T j(1); j < N; ++j)
-        rst = (v[j] < rst)?v[j]:rst;
-    return rst;
-}
-
-template <typename T>
-T max(const std::vector<T>& v)
-{
-	// find the max element in the std::vector 
-    const SIZE_T N(v.size());
-    T rst = v[0];
-    for(SIZE_T j(1); j < N; ++j)
-        rst = (v[j] > rst)?v[j]:rst;
-    return rst;
+	// ~v => extract unit vector along v1 direction
+	std::vector<T1> rst(v1);
+	
+	return rst;
 }
 
 // other utilities
